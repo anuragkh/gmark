8c59e6f6ccee92a97ca4a5509b611a0d95e05ae6
Anurag Khandelwal <anuragk@berkeley.edu>
2017-02-14 13:44:48 -0800

    Updated script for generating social dataset

--- demo/scripts/social.sh
+++ demo/scripts/social.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-folder="../demo/social/"
+folder=${1:-"../demo/social/"}
 
 cd ../../src
 ./test -c ../use-cases/social-network.xml -g "$folder"social-graph.txt -w "$folder"social-workload.xml -r "$folder"
+mkdir -p "$folder"raw
+awk '{ print $1 ",p" $2 "," $3 "," systime() }' "$folder"social-graph.txt > "$folder"raw/edges.txt
+max=`awk 'BEGIN { max = 0 } { if ($1 > max) { max = $1 } if ($3 > max) { max = $3 } } END { print max }' "$folder"social-graph.txt`
+seq 0 $max > "$folder"raw/nodes.txt
 
 cd querytranslate
 ./test -w ../"$folder"social-workload.xml -o ../"$folder"social-translated
