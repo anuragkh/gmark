ecf4461db1c12ef23b9af23038436f807235193a
Anurag Khandelwal <anuragk@berkeley.edu>
2017-02-18 20:01:47 +0000

    Updated query generator script

--- demo/scripts/social.sh
+++ demo/scripts/social.sh
@@ -2,15 +2,13 @@
 
 folder=${1:-"../demo/social/"}
 
-cd ../../src
-./test -c ../use-cases/social-network.xml -g "$folder"social-graph.txt -w "$folder"social-workload.xml -r "$folder"
-mkdir -p "$folder"raw
-awk '{ print $1 ",p" $2 "," $3 "," systime() }' "$folder"social-graph.txt > "$folder"raw/edges.txt
-max=`awk 'BEGIN { max = 0 } { if ($1 > max) { max = $1 } if ($3 > max) { max = $3 } } END { print max }' "$folder"social-graph.txt`
-seq 0 $max > "$folder"raw/nodes.txt
+cd ../../src/querytranslate
 
-cd querytranslate
-./test -w ../"$folder"social-workload.xml -o ../"$folder"social-translated
+for num_nodes in 500000 1000000 2000000 4000000 8000000; do
+  data_dir="${folder}${num_nodes}/"
+  mkdir -p $data_dir
 
-cd ../queryinterface
-./test -w ../"$folder"social-workload.xml -t ../"$folder"social-translated -o ../"$folder"social-interface
+  sed -i "s/<nodes>.*<\\/nodes>/<nodes>$num_nodes<\\/nodes>/g" ../use-cases/social-network.xml
+  mkdir -p ../"$data_dir"social-translated
+  ./test -w ../"$folder"social-workload.xml -o ../"$data_dir"social-translated
+done
